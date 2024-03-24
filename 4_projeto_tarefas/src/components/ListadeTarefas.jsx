ceeca5aa3a995bc7d5da2c5c028bbfe202887040
Matt <mateuservico@gmail.com>
2024-03-24 19:59:29 -0300

    inicio_task_list

---
+++ 4_projeto_tarefas/src/components/ListadeTarefas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListadeTarefas = () => {
+  return (
+    <div>ListadeTarefas</div>
+  )
+}
+
+export default ListadeTarefas
---
+++ 4_projeto_tarefas/src/components/TarefaInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TarefaInput = () => {
+  return (
+    <div>TarefaInput</div>
+  )
+}
+
+export default TarefaInput
