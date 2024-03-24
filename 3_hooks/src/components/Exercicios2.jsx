ca446355cace06ce77c01c4023a59a5b021f810a
Matt <mateuservico@gmail.com>
2024-03-24 18:10:18 -0300

    conclusao_exercicios

--- 3_hooks/src/components/Exercicios2.jsx
+++ 3_hooks/src/components/Exercicios2.jsx
@@ -1,6 +1,6 @@
-import {useEffect} from 'react'
+import {useEffect, useMemo, useState} from 'react'
 
-// EXERCICIO 1 - EXIBIR INFORMACOES DE UM USUARIO UTILIZANDO PROPS
+// EXERCICIO 1 -----> EXIBIR INFORMACOES DE UM USUARIO UTILIZANDO PROPS
 
 const UsuarioInfo = ({userInfo}) => {
 
@@ -21,6 +21,70 @@ const UsuarioInfo = ({userInfo}) => {
 
 }
 
+// --MEMORIZAR CALCULOS COM "USEMEMO"
+// EXERCICIOS 2 ----->
+const fibbonaci = (n) => {
+    if (n <=1){
+        return n
+    }
+
+    return fibbonaci(n - 1) + fibbonaci(n - 2);
+}
+const FIbbonaciCalculadora = ({num}) =>{
+
+        const fibResult = useMemo(() => fibbonaci(num), [num])
+
+        return(
+            <div><p>Fibbonaci de {num} é {fibResult}</p></div>
+        )
+}
+
+
+
+// EXERCÍCIO ---- 3 
+
+// CRIAR UM CUSTOM HOOK, QUE VEJA SE ESTÃOI ONLINE OU OFFLINE:
+
+// Dessa vez, o HOOK VAI SER CRIADO INTERNAMENTE  NESSE ARQUIVO !!!
+
+
+const useOnlineStatus = () =>{
+
+    const[estaON, setEstaON] = useState(navigator.onLine)
+
+    useEffect(() =>{
+
+        const handleOnline  = () => setEstaON(true)
+        const handleOffLine = () => setEstaON(false)
+
+        window.addEventListener('online', handleOnline)
+        window.addEventListener('offline', handleOffLine)
+
+        //limpeza
+        return() => {
+            window.addEventListener('online', handleOnline)
+            window.addEventListener('offline', handleOffLine)
+        }
+
+
+    }, []);
+    return estaON;
+};
+const IndicaStatusOnline = () =>{
+
+    const estaON = useOnlineStatus();
+
+
+    return (
+        <div>
+            <p>Você está: {estaON? 'Online' : 'Offline'}</p>
+        </div>
+    )
+}
+
+
+
+
 
 
 const Exercicios2 = () => {
@@ -31,8 +95,12 @@ const Exercicios2 = () => {
 
   return (
     <div>
-        <h2>Exercicios 1</h2>
+        <h2>Exercício 1:</h2>
         <UsuarioInfo userInfo={userInfo}/>
+        <h2>Exercício 2:</h2>
+        <FIbbonaciCalculadora num={10}/>
+        <h2>Exercício 3:</h2>
+        <IndicaStatusOnline/>
         </div>
     
   )
