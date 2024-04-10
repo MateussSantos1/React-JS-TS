07a1cbc5582756b7ece0bc8c4f8dfacb11f86e5f
Matt <mateuservico@gmail.com>
2024-04-10 16:58:43 -0300

    estilos_inciais

--- 8_album_fotos/src/components/SearchBar.jsx
+++ 8_album_fotos/src/components/SearchBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const SearchBar = () => {
+
+const categorias = [
+  "Natureza",
+  "Pessoas",
+  "Mundo Tech",
+  "Animais",
+  "Esportes"
+]
+
   return (
-    <div>SearchBar</div>
+    <div className="search-bar">
+
+      <input type="text"  placeholder='pesquisar'/>
+      <button>Pesquisar</button>
+      <select>
+      {categorias.map((categoria) =>(
+
+        <option key={categoria}  value={categoria}>{categoria}</option>
+
+      ))}
+      </select>
+      </div>
   )
 }
 
