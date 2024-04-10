4ceb1a443504499dd3cd673454d3ea2f500b2579
Matt <mateuservico@gmail.com>
2024-04-10 20:45:46 -0300

    finalizei_album

--- 8_album_fotos/src/components/FotoAmpliada.jsx
+++ 8_album_fotos/src/components/FotoAmpliada.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const FotoAmpliada = ({foto, setFotoAmpliada}) => {
   return (
-    <div className="foto-ampliada-backdrop" onClick={() => setFotoAmpliada(null)}>
+    <div className="foto-ampliada-backdrop">
       <div className="foto-ampliada-container">
-
         {/* traz a foto em tamanho real o f=urls.regular !!! */}
         <img src={foto.urls.regular} alt={foto.alt_description}/>
+        <p className='fechar' onClick={(()=> setFotoAmpliada(null))}>Fechar</p>
       </div>
 
     </div>
