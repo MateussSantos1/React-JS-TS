cd75f0858b79be629c09780869cf16fea19c131f
Matt <mateuservico@gmail.com>
2024-04-10 16:32:21 -0300

    configurando_api_album_fotos

---
+++ 8_album_fotos/src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
+import PhotoList from './components/PhotoList'
+import FotoAmpliada from './components/FotoAmpliada'
+function App() {
+
+  const [query, setQuery] = useState("")
+  const[categoria, setCategoria] = useState("")
+
+
+const  fetchData = async({query}, {categoria}) =>{
+
+  const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY
+
+  const response = await axios.get('https://api.unsplash.com/photos/random', {
+    params: {
+      client_id: apiKey
+    }
+  }
+
+)
+
+console.log(response)
+
+}
+
+useEffect (()=>{
+  fetchData(query, categoria)
+}, []
+
+)
+
+  return (
+    <div className='container'>
+<SearchBar/>
+<PhotoList/>
+<FotoAmpliada/>
+
+    </div>
+  )
+}
+
+export default App
