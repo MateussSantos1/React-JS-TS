070fa888a5aa95ce2d0b74d15ce4b7d7f588da4b
Matt <mateuservico@gmail.com>
2024-04-12 10:55:43 -0300

    ts_config.jsom

--- 9_ts/dist/hello.js
+++ 9_ts/dist/hello.js
@@ -1,4 +1,3 @@
-console.log("ola-typescript")
-
+console.log("ola-typescript");
 // após digitar tsc--watch, todas as alterações do ARQUIVO TSC  serão instantaneamente alteradas NO JS
 //isso é regido pelo arquivo tsconfig.jon 
