06be31109fc0a89f5765f27e2d8be05b8b53f35c
Matt <mateuservico@gmail.com>
2024-04-12 11:14:18 -0300

    tipos_básicos_ts

---
+++ 9_ts/dist/tipos.js
@@ -0,0 +1,24 @@
+//OBS: Se ocorrer algum errona compiação, se corrige o arquivo TS e não o JS !!!!
+// Tipos Básicos:
+// 1 - Valor Booleano:
+let estáAtivo;
+estáAtivo = true;
+// 2 - Valor Numérico:
+let total = 777;
+//ou
+//let total: number
+//total = 777
+// 3 - Valor em Texto:
+let meuNome = "Mateus";
+//ou
+//let meuNome: string
+//meuNome = "Mateus"
+// Consoles dessa variaveis:
+//Seus Tipos:
+console.log(typeof estáAtivo);
+console.log(typeof meuNome);
+console.log(typeof total);
+//Seus Valores:
+console.log(estáAtivo);
+console.log(meuNome);
+console.log(total);
---
+++ 9_ts/src/tipos.ts
@@ -0,0 +1,51 @@
+//OBS: Se ocorrer algum errona compiação, se corrige o arquivo TS e não o JS !!!!
+
+
+// Tipos Básicos:
+
+
+// 1 - Valor Booleano:
+
+let estáAtivo : boolean;
+
+estáAtivo = true;
+
+
+// 2 - Valor Numérico:
+
+let total: number = 777;
+
+//ou
+
+//let total: number
+//total = 777
+
+
+// 3 - Valor em Texto:
+let meuNome: string = "Mateus";
+
+//ou
+
+//let meuNome: string
+//meuNome = "Mateus"
+
+
+// Consoles dessa variaveis:
+
+
+//Seus Tipos:
+console.log(typeof estáAtivo)
+console.log(typeof meuNome)
+console.log(typeof total)
+
+
+//Seus Valores:
+
+console.log(estáAtivo)
+console.log(meuNome)
+console.log(total)
+
+
+
+
+
