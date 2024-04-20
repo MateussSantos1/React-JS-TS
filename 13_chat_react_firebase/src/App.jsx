931b413e0f6f31620bcb02d1d03312bfc71bbf59
Matt <mateuservico@gmail.com>
2024-04-19 21:58:46 -0300

    fim do chat

--- 13_chat_react_firebase/src/App.jsx
+++ 13_chat_react_firebase/src/App.jsx
@@ -1,12 +1,24 @@
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
-
+import "./App.css"
+import { useAuthState } from 'react-firebase-hooks/auth'
+import {auth} from "./services/firebaseConfig"
+import { addDoc, collection, limit, orderBy, query, serverTimestamp } from 'firebase/firestore'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+import { useState } from 'react'
+import { app, databaseApp } from './services/firebaseConfig'
 export const App = () => {
+  const[user] = useAuthState(auth)
   return (
-    <div className="app">
+    <div className="App">
       <header>
         <h1>React Chat</h1>
         <SignOut/>
       </header>
+      <section>
+
+        {/* se tiver logado, mostra o chat, se nao, mostra o componente para logar ! */}
+        {user ? <ChatRoom/> : <SignIn/>}
+      </section>
     </div>
   )
 }
@@ -14,7 +26,69 @@ export const App = () => {
 
 // Salas de conversa:
 export const ChatRoom = () => {
-  return <h1>ChatRoom</h1>
+
+  //Referencia da tabela message
+  const messageRef = collection(databaseApp, 'messages')
+
+
+  //ordenando as mensagens pela ordem de envio/criacao
+  const QueryMessages = query(messageRef, orderBy("createdAt"), limit(50))
+
+  const [messages] = useCollectionData(QueryMessages, { idField: 'id' });
+
+
+  const [formValue, setFormValue] = useState("")
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+
+    const {photoURL, uid} = auth.currentUser;
+
+    await addDoc(messageRef, {
+      text: formValue,
+      uid,
+      photoURL,
+      createdAt: serverTimestamp()
+    })
+    setFormValue("")
+
+  }
+
+
+    return(
+      <>
+      <main>
+        {messages && messages.map((msg, index) =>
+        (<ChatMessage key={index} message = {msg}/>))}
+      </main>
+
+          <form onSubmit={sendMessage}>
+          <input type="text" value={formValue} onChange={e => setFormValue(e.target.value)}></input>
+          <button>Enviar</button>
+
+          </form>
+
+      
+      </>
+    )
+}
+
+//MENSAGEM : 
+
+export const ChatMessage = (props) => {
+  const {text, photoURL, uid} = props.message;
+  
+
+  //estilo do css, se for minha mensagem tem um estilo
+  // sinao for tem outro
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 
@@ -33,5 +107,5 @@ export const SignIn = () => {
 //DESLOGAR
 
 export const SignOut= () => {
-  return <h1>SignOut</h1>
+  return auth.currentUser && <button className='sign-out' onClick={() => auth.signOut()}>Sair</button>
 }
